0e36d6da8cf1e5523794625702b192e7de6be40c
muonroi <leanhphi1706@gmail.com>
2026-01-10 16:46:14 +0700

    chore: bump version to 1.6.24 and add new permission enums

--- src/Muonroi.BaseTemplate.Data/Config/Permission.cs
+++ src/Muonroi.BaseTemplate.Data/Config/Permission.cs
@@ -19,6 +19,7 @@
         Auth_GetPermissions = 1L << 6,
         Auth_GetRolePermissions = 1L << 7,
         Auth_GetRoleUsers = 1L << 8,
+        Auth_All = 1L << 9,
         Auth_AssignRoleUser = 1L << 11,
         Auth_RemovePermissionFromRole = 1L << 12,
         Sample_Create = 1L << 13,
@@ -26,6 +27,13 @@
         Notification_Create = 1L << 15,
         Notification_GetAll = 1L << 16,
         Container_Create = 1L << 17,
+        Admin_Auth_Dashboard = 1L << 18,
+        Admin_Roles_View = 1L << 19,
+        Admin_Roles_Create = 1L << 20,
+        Admin_Roles_Edit = 1L << 21,
+        Admin_Roles_Users = 1L << 22,
+        Users_Manage = 1L << 23,
+        Rules_Configure = 1L << 24,
     }
 
 }
